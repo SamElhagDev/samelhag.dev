b078fec480288219ed01978b7a1cc292391197f3
Sam Elhag <sami.eltaj.elhag@gmail.com>
2026-03-01 01:19:50 -0500

    Replaced 3 functions (getSessionFlag, setSessionFlag, reloadPage) with a single waitForKatexThenRender() promise Removed dead renderKatex() Removed 2 debug console.log statements; Removed inline <script> for renderKatexInDocument (duplicate); simplified OnAfterRenderAsync Removed unneeded usings

--- SamElhagPersonalSite/wwwroot/js/heatSimulation.js
+++ SamElhagPersonalSite/wwwroot/js/heatSimulation.js
@@ -471,27 +471,5 @@ window.HeatSimulation = {
 
         initializeSimulation();
         animate();
-    },
-
-    renderKatex: function () {
-        if (typeof renderMathInElement === 'function' && typeof katex !== 'undefined') {
-            renderMathInElement(document.body, {
-                delimiters: [
-                    { left: "$$", right: "$$", display: true },
-                    { left: "$", right: "$", display: false }
-                ],
-                throwOnError: false
-            });
-            var nusseltEl = document.getElementById('nusseltFormula');
-            if (nusseltEl && !nusseltEl.hasChildNodes()) {
-                katex.render(
-                    "Nu = \\begin{cases} \\underbrace{0.664 \\, Re^{1/2} \\, Pr^{1/3}}_{\\text{Laminar (Blasius solution)}} & Re < 5 \\times 10^5 \\\\ \\underbrace{0.037 \\, Re^{4/5} \\, Pr^{1/3}}_{\\text{Turbulent (Dittus-Boelter type)}} & Re \\geq 5 \\times 10^5 \\end{cases}",
-                    nusseltEl,
-                    { displayMode: true, throwOnError: false }
-                );
-            }
-        } else {
-            setTimeout(function () { window.HeatSimulation.renderKatex(); }, 200);
-        }
     }
 };
